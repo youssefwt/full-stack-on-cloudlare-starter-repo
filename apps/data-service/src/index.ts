b35c8c147e5c17b4eec468b7ce70dd979dcf6986
Matthew Sessions <matthewlsessions@gmail.com>
2025-07-22 13:12:08 -0700

    changed name

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,6 +1,6 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
-export default class MessagingService extends WorkerEntrypoint<Env> {
+export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
 		return new Response('Hello World!');
 	}
