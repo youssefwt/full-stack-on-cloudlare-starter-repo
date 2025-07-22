7f7c01ed94dd2af4d704f68205f8f985cf50af55
Matthew Sessions <matthewlsessions@gmail.com>
2025-07-22 13:15:10 -0700

    removed service bindings

--- apps/data-service/service-bindings.d.ts
+++ apps/data-service/service-bindings.d.ts
@@ -1,14 +1,1 @@
-interface DestinationStatusEvaluationParams {
-	linkId: string;
-	destinationUrl: string;
-	accountId: string;
-}
-
-interface ClickBatchEtlParams {
-	dayDate: string;
-}
-
-interface Env extends Cloudflare.Env {
-	CLICK_ETL_WORKFLOW: Workflow<ClickBatchEtlParams>;
-	DESTINATION_STATUS_EVALUATION: Workflow<DestinationStatusEvaluationParams>;
-}
+interface Env extends Cloudflare.Env {}
