83bf7d0b04165cbdd352710cab32b665c0a289d1
youssefw <ywtalaat@gmail.com>
2025-10-16 20:42:18 +0300

    adding kv cache store

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,4 +1,4 @@
-import { getDestinationForCountry } from '@/helpers/route-ops';
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { getLink } from '@repo/data-ops/queries/links';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 
@@ -12,7 +12,7 @@ export const App = new Hono<{ Bindings: Env }>();
 App.get('/:id', async (c) => {
     const id = c.req.param('id');
 
-    const linkInfo = await getLink(id)
+    const linkInfo = await getRoutingDestinations(c.env, id)
     if (!linkInfo) {
 		return c.text('Destination not found', 404);
 	}
@@ -23,7 +23,7 @@ App.get('/:id', async (c) => {
     }
 
     const headers = cfHeader.data
-    console.log(headers)
     const destination = getDestinationForCountry(linkInfo, headers.country)
+
     return c.redirect(destination)
 })
