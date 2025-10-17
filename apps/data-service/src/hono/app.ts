088134c318a658e79a70f66a171cb75ace9ab9fd
youssefw <ywtalaat@gmail.com>
2025-10-17 14:27:20 +0300

    implement queues for handling link clicks

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,6 +1,7 @@
 import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { getLink } from '@repo/data-ops/queries/links';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 
 import { Hono } from 'hono';
 
@@ -14,16 +15,33 @@ App.get('/:id', async (c) => {
 
     const linkInfo = await getRoutingDestinations(c.env, id)
     if (!linkInfo) {
-		return c.text('Destination not found', 404);
-	}
+        return c.text('Destination not found', 404);
+    }
 
     const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf)
     if (!cfHeader.success) {
-		return c.text('Invalid Cloudflare headers', 400);
+        return c.text('Invalid Cloudflare headers', 400);
     }
 
     const headers = cfHeader.data
     const destination = getDestinationForCountry(linkInfo, headers.country)
+
+    const queueMessage: LinkClickMessageType = {
+        "type": "LINK_CLICK",
+        data: {
+            id: id,
+            country: headers.country,
+            destination: destination,
+            accountId: linkInfo.accountId,
+            latitude: headers.latitude,
+            longitude: headers.longitude,
+            timestamp: new Date().toISOString()
+        }
+    }
+    // don't kill the worker after the redirect right away, gives time until we execute the queue send
+    c.executionCtx.waitUntil(
+        c.env.QUEUE.send(queueMessage)
+    )
 
     return c.redirect(destination)
 })
